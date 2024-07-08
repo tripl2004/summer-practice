f59812e585fdfee9ef6360c74d923f7647f044e6
tripl2004 <timazx2004@gmail.com>
2024-07-08 23:05:56 +0300

    solving the integral by the trapezoid method

--- day_00/ex_02.py
+++ day_00/ex_02.py
@@ -1,11 +1,23 @@
 import math
-r=1
-l=0
-n=1000
-h=(r-l)/n
-integral=0
-for i in range(n):
-    x=i*h
-    integral+=math.exp(x+(h/2))*h
+def f(x):
+    return math.exp(x)
 
-print(integral)
+def Integral(l, r, n):
+    r=1
+    l=0
+    h=(r-l)/n
+    integral=0
+    for i in range(n):
+        x=i*h
+        integral+=f(x+(h/2))*h
+
+    print(n,abs(integral-(f(1)-1)))
+
+    
+left=0
+right=1
+n=10
+
+for _ in range (7):
+    Integral(left, right, n)
+    n *= 10
