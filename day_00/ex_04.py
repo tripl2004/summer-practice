667cdfeecfb5abd62b5e324d5fb7c9346f793e46
tripl2004 <timazx2004@gmail.com>
2024-07-08 23:07:10 +0300

    solving the integral by the Simpson method

--- day_00/ex_04.py
+++ day_00/ex_04.py
@@ -1,19 +1,31 @@
 import math
-r=1
-l=0
-n=1000
-if n%2!=0:
-    raise ValueError
-h=(r-l)/n
-integral=math.exp(l)+math.exp(r)
-for i in range(1,n):
-    x=i*h
-    if i%2==0:
-        integral+=2*math.exp(x)
-    else:
-        integral+=4*math.exp(x)
+def f(x):
+    return math.exp(x)
 
-integral *= h/3
+def Integral(a, b, n):
+    r=1
+    l=0
+    if n%2!=0:
+        raise ValueError
+    h=(r-l)/n
+    integral=math.exp(l)+math.exp(r)
+    for i in range(1,n):
+        x=i*h
+        if i%2==0:
+            integral+=2*math.exp(x)
+        else:
+            integral+=4*math.exp(x)
+
+    integral *= h/3
 
 
-print(integral)
+    print(n,abs(integral-(f(1)-1)))
+
+    
+left=0
+right=1
+n=10
+
+for _ in range (7):
+    Integral(left, right, n)
+    n *= 10
